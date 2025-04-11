61e584767ff182305304e8d7eb618d7463474b9c
Al-Amin49 <alamin496832@gmail.com>
2025-04-11 17:22:25 +0600

    react with tw

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <div className=" flex justify-center items-center min-h-screen">
+   <h3 className="tex-xl lg:text-3xl text-green-500 bg-indigo-100 p-12 rounded-md">React with Tailwind CSS</h3>
+    </div>
+  )
+}
+
+export default App
